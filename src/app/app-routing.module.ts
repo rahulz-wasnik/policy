0c7edf6b28bc9b323d5818b5d7686d02236d36ab
Rahul Wasnik <rahulz.wasnik@gmail.com>
2023-03-28 15:22:43 +0530

    Adding error page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 const routes: Routes = [
-  { path: 'add-policy', loadChildren: () => import('./add-policy/add-policy.module').then(m => m.AddPolicyModule) }, 
-  { path: 'view-modify-policy', loadChildren: () => import('./view-modify-policy/view-modify-policy.module').then(m => m.ViewModifyPolicyModule) },
-  { path: 'view-policy-history', loadChildren: () => import('./view-policy-history/view-policy-history.module').then(m => m.ViewPolicyHistoryModule) }];
+  { 
+    path: 'add-policy', 
+    loadChildren: () => import('./add-policy/add-policy.module').then(m => m.AddPolicyModule) 
+  }, 
+  { 
+    path: 'view-modify-policy',
+    loadChildren: () => import('./view-modify-policy/view-modify-policy.module').then(m => m.ViewModifyPolicyModule) 
+  },
+  { 
+    path: 'view-policy-history',
+    loadChildren: () => import('./view-policy-history/view-policy-history.module').then(m => m.ViewPolicyHistoryModule)
+  },
+  { path: 'error', component: ErrorPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
